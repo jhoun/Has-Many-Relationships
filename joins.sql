3bf3b2019c7887d636b42d02118d8d66c31ebe2d
jhoun <jayhoun@gmail.com>
2017-01-14 22:41:10 -1000

    created query by selecting username using when user created account

--- joins.sql
+++ joins.sql
@@ -11,3 +11,23 @@ SELECT first_name, last_name, posts.*
   INNER JOIN posts
   ON posts.users_id = users.id
   WHERE users_id = 200;
+
+SELECT username, posts.*
+  FROM users
+  INNER JOIN posts
+  ON posts.users_id = users.id
+  WHERE first_name = 'Norene'
+  AND last_name = 'Schmitt';
+
+SELECT users.username
+  FROM users
+  INNER JOIN posts
+  ON posts.users_id = users.id
+  WHERE posts.create_at > 'January 1, 2015';
+
+
+SELECT posts.title, posts.content, users.username
+  FROM users
+  INNER JOIN posts
+  ON posts.users_id = users.id
+  WHERE users.create_at > 'January 1, 2015';
