e717fa71a0628bb7550be81a00d10cee2146aa96
jhoun <jayhoun@gmail.com>
2017-01-14 22:06:28 -1000

    created query that gets username from users and all fields from posts

--- joins.sql
+++ joins.sql
@@ -4,3 +4,10 @@ SELECT *
 SELECT *
   FROM posts
   WHERE users_id = 100;
+
+
+SELECT first_name, last_name, posts.*
+  FROM users
+  INNER JOIN posts
+  ON posts.users_id = users.id
+  WHERE users_id = 200;
